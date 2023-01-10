1d81ba4169a3b1e4710bb9fb9805019819a52f2e
d_nekh <dnekh.dev@gmail.com>
2023-01-11 06:20:10 +0900

    This commit adds a new html template 'about' and tests it by 'aboutUs' method of Maincontroller class

--- src/main/java/com/dnekh/demowebshop/controllers/MainController.java
+++ src/main/java/com/dnekh/demowebshop/controllers/MainController.java
@@ -3,13 +3,21 @@ package com.dnekh.demowebshop.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index(Model model){
         model.addAttribute("name", "Mors");
         return "index";
+    }
+
+    @GetMapping("/about-us")
+    public String aboutUs(@RequestParam(name="userName", required=false, defaultValue = "customer")
+                              String userName, Model model) {
+        model.addAttribute("name", userName);
+        return "about";
     }
 }
